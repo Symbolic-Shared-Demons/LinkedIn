a043d53a715118b87fdaca33501091a00df4d189
mdevon19 <57203156+mdevon19@users.noreply.github.com>
2020-08-25 21:13:54 -0400

    Update travis_script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -1,4 +1,15 @@
 git config --global pull.rebase false
 git checkout master
 git pull
-cd .**
+cd ./Project2
+
+mvn clean test -Dtest=**Insert testing class**
+
+if [[ "$?" -ne 0 ]]
+then
+  echo The build failed!
+  exit 100
+else
+  echo All tests pass! 
+  #****have docker start somehow here****
+fi
